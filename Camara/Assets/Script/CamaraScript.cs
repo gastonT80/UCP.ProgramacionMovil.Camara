7dae861f7062f652e2299c363a280295fce9c383
GastonT80 <208198098+gastonT80@users.noreply.github.com>
2025-04-25 15:29:14 -0300

    actualizacion de camar

--- Camara/Assets/Script/CamaraScript.cs
+++ Camara/Assets/Script/CamaraScript.cs
@@ -2,21 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class CamaraScript : MonoBehaviour
 {
     private bool camaraEnabled;
-    private WebCamTexture cam;
-    private Texture Ground;
+    private WebCamTexture backcam;
+    private Texture deaultBackGround;
 
-    private RawImage backgound;
+    public RawImage backgound;
 
-    public AspectRatioFitter fitter;
+    public AspectRatioFitter fit;
 
     // Start is called before the first frame update
     void Start()
     {
-        Ground = backgound.texture;
+        deaultBackGround = backgound.texture;
         WebCamDevice[]devices=WebCamTexture.devices;
 
         if (devices.Length==0)
@@ -27,14 +28,39 @@ public class CamaraScript : MonoBehaviour
         }
 
         for (int i = 0; i < devices.Length; i++) 
-        { 
-            
+        {
+            if (!devices[i].isFrontFacing)
+            {
+                backcam = new WebCamTexture(devices[i].name,Screen.width,Screen.height);
+            }
         }
+
+        if (backcam == null)
+        {
+            Debug.Log("no se encuentra la camara trasera");
+            return;
+        }
+
+        backcam.Play();
+        backgound.texture = backcam;
+        camaraEnabled=true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!camaraEnabled)
+        {
+            return;
+        }
+
+        float ratio=(float)backcam.width/ (float)backcam.height;
+        fit.aspectRatio = ratio;
+
+        float scaleY= backcam.videoVerticallyMirrored ? -1f : 1f;
+        backgound.rectTransform.localScale = new Vector3(1f, scaleY, 1f);
+
+        float orient= backcam.videoRotationAngle;
+        backgound.rectTransform.localEulerAngles = new Vector3(0, 0, orient);
     }
 }
